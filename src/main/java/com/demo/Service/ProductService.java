e4f0100f8aa31749ed60f524469ca4b88a4406bc
KundanChourasiya <mailmekundanchourasiya@gmail.com>
2025-04-21 03:29:16 +0530

    Updated code

--- src/main/java/com/demo/Service/ProductService.java
+++ src/main/java/com/demo/Service/ProductService.java
@@ -10,9 +10,9 @@ public interface ProductService {
     // GET: Retrieve all Product
     public List<Product> getAllProduct();
 
-
     // POST: Save a new Product
     public Product saveProduct(Product product);
 
+    // GET: Generate a product list Report
     public ByteArrayInputStream generatePdf();
 }
